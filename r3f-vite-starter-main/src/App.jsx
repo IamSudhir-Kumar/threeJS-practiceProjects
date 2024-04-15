5812f142b619447fddd8cef79098fc8fb4f02b98
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-04-15 22:44:24 +0530

    Add @theatre/core, @theatre/r3f, and @theatre/studio dependencies

--- r3f-vite-starter-main/src/App.jsx
+++ r3f-vite-starter-main/src/App.jsx
@@ -1,11 +1,16 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 
+
 function App() {
   return (
+   
     <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+     
+      <axesHelper args={[5]} />
+      <color attach="background" args={["grey"]} />
       <Experience />
+     
     </Canvas>
   );
 }
--- r3f-vite-starter-main/src/components/Experience.jsx
+++ r3f-vite-starter-main/src/components/Experience.jsx
@@ -1,14 +1,23 @@
 import { OrbitControls } from "@react-three/drei";
 import { Model } from "./object";
+import studio from '@theatre/studio'
+import extension from '@theatre/r3f/dist/extension'
+
+
+studio.initialize()
+studio.extend(extension)
 
 export const Experience = () => {
   return (
     <>
+  
       <OrbitControls />
-      <mesh>
+      <mesh >
         <Model />
+        <ambientLight intensity={0.8} />
         <meshNormalMaterial />
       </mesh>
+    
     </>
   );
 };
