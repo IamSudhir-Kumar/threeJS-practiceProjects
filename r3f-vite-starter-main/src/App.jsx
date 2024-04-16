c4d3617c3261f14fde51a5dd7f274bcb48ed9d24
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-04-16 09:31:18 +0530

    sheet intiated in the scene

--- r3f-vite-starter-main/src/App.jsx
+++ r3f-vite-starter-main/src/App.jsx
@@ -1,16 +1,21 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { SheetProvider } from '@theatre/r3f'
+import { getProject } from '@theatre/core'
+import { PerspectiveCamera } from '@theatre/r3f'
 
+const demoSheet = getProject('Demo Project').sheet('Demo Sheet')
 
 function App() {
   return (
    
     <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-     
+       <SheetProvider sheet={demoSheet}>
+       <PerspectiveCamera theatreKey="Camera" makeDefault position={[5, 5, -5]} fov={75} />
       <axesHelper args={[5]} />
       <color attach="background" args={["grey"]} />
       <Experience />
-     
+     </SheetProvider>
     </Canvas>
   );
 }
