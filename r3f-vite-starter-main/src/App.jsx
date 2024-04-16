39dfe3cfcf3ce279ef5efa887154e98c6d37272f
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-04-16 11:28:43 +0530

    Update camera position and field of view in App.jsx

--- r3f-vite-starter-main/src/App.jsx
+++ r3f-vite-starter-main/src/App.jsx
@@ -1,15 +1,20 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { SheetProvider } from '@theatre/r3f'
+import * as core from '@theatre/core'
 import { getProject } from '@theatre/core'
 import { PerspectiveCamera } from '@theatre/r3f'
 
+
 const demoSheet = getProject('Demo Project').sheet('Demo Sheet')
+
 
 function App() {
   return (
    
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <Canvas 
+    shadows 
+    camera={{ position: [7.05, 2.89, -6.68], fov: 75 }}>
        <SheetProvider sheet={demoSheet}>
        <PerspectiveCamera theatreKey="Camera" makeDefault position={[5, 5, -5]} fov={75} />
       <axesHelper args={[5]} />
